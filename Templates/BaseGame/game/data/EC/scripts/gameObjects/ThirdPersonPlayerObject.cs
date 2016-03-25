9be8571ec1be37286a3123439f4ea121b7a8c302
RoundedIcon <alphaslime@gmail.com>
2016-03-25 14:41:52 -0600

    Test Changes - Script side
    Basic workings for script networking and a very floaty character

--- Templates/BaseGame/game/data/EC/scripts/gameObjects/ThirdPersonPlayerObject.cs
+++ Templates/BaseGame/game/data/EC/scripts/gameObjects/ThirdPersonPlayerObject.cs
@@ -1,7 +1,7 @@
 function ThirdPersonPlayerObject::onAdd(%this)
 {
    %this.turnRate = 0.3;
-
+/*
    %this.phys = %this.getComponent("PlayerControllerComponent");
    %this.collision = %this.getComponent("CollisionComponent");
    %this.cam = %this.getComponent("CameraComponent");
@@ -9,7 +9,7 @@ function ThirdPersonPlayerObject::onAdd(%this)
    %this.animation = %this.getComponent("AnimationComponent");
    %this.stateMachine = %this.getComponent("StateMachineComponent");
    %this.mesh = %this.getComponent("MeshComponent");
-
+*/
    %this.stateMachine.forwardVector = 0;
 
    %this.crouch = false;
@@ -27,9 +27,17 @@ function ThirdPersonPlayerObject::onAdd(%this)
    %this.aimedMaxPitch = 90;
    %this.aimedMinPitch = -90;
    
+   %this.airControl = 0.3;
+   
    %this.arms = SGOManager.spawn("FirstPersonArms", true);
    
    %this.add(arms);
+   
+   %this.pending = true;
+   
+   %this.counter = 0;
+   
+   //%this.camOrb = %this.camArm;
    
    //%this.mesh.mountObject(%this.arms, "Eye");
 }
@@ -42,6 +50,8 @@ function ThirdPersonPlayerObject::onRemove(%this)
 function ThirdPersonPlayerObject::moveVectorEvent(%this)
 {
     %moveVector = %this.getMoveVector();
+    
+    //echo(%moveVector);
 
     // forward of the camera on the x-z plane
     %cameraForward = %this.cam.getForwardVector();
@@ -96,6 +106,8 @@ function ThirdPersonPlayerObject::moveVectorEvent(%this)
       }
    }
    
+   //%this.phys.setInputVelocity(%moveVec * 10);
+   
    if(%this.crouch)
       %this.phys.inputVelocity = VectorScale(%this.phys.inputVelocity, %this.crouchSpeedMod);
 }
@@ -103,24 +115,22 @@ function ThirdPersonPlayerObject::moveVectorEvent(%this)
 function ThirdPersonPlayerObject::moveYawEvent(%this)
 {
    %moveRotation = %this.getMoveRotation();
-
-    %camOrb = %this.getComponent("CameraOrbiterComponent");
     
     if(%this.aiming || %this.firstPerson)
     {
       %this.rotation.z += %moveRotation.z * 10;
     }
 
-    %camOrb.rotation.z += %moveRotation.z * 10;
+    %this.camArm.rotation.z += %moveRotation.z * 10;
 }
 
 function ThirdPersonPlayerObject::movePitchEvent(%this)
 {
    %moveRotation = %this.getMoveRotation();
+   
+   //%camOrb = %this.camArm;
 
-    %camOrb = %this.getComponent("CameraOrbiterComponent");
-
-    %camOrb.rotation.x += %moveRotation.x * 10;
+   %this.camArm.rotation.x += %moveRotation.x * 10;
 }
 
 function ThirdPersonPlayerObject::moveRollEvent(%this){}
@@ -171,13 +181,17 @@ function ThirdPersonPlayerObject::moveTriggerEvent(%this, %triggerNum, %triggerV
 	   {
 	      %surfaceNormal = %this.collision.getCollisionNormal(0);
 	      %jumpVector = VectorScale(%surfaceNormal, 200);
+	      %jumpVector = VectorAdd(%jumpVector, "0 0 300");
 	      echo("Jump surface Angle is at: " @ %surfaceNormal);
 	      
 	      %this.phys.applyImpulse(%this.position, %jumpVector);
-	      %this.setForwardVector(%jumpVector);
+	      %this.setForwardVector(%jumpVector.x SPC %jumpVector.y SPC "0");
 	   }
       else
+      {
          %this.phys.applyImpulse(%this.position, "0 0 300");
+         echo(%collisionAngle);
+      }
 	}
 	else if(%triggerNum == 4)
 	{
@@ -209,11 +223,10 @@ function ThirdPersonPlayerObject::onCollisionEvent(%this, %colObject, %colNormal
     echo(%this @ " collided with " @ %colObject);
 }
 
-function ThirdPersonPlayerObject::processTick(%this)
+function ThirdPersonPlayerObject::updateEnt(%this)
 {
    %moveVec = %this.getMoveVector();
    %bestFit = "";
-   
    if(%this.crouch)
    {
       if(%moveVec.x != 0 || %moveVec.y != 0)
@@ -228,9 +241,14 @@ function ThirdPersonPlayerObject::processTick(%this)
       else
          %bestFit = "Root";
    }
-   
+
    if(%this.animation.getThreadAnimation(0) !$= %bestFit)
       %this.animation.playThread(0, %bestFit);
+      
+   //%camOrb = %this.getComponent("CameraOrbiterComponent");
+   //%camOrb.rotation.z += 0.1;
+   if (%this.isMethod( thingy ))
+      %this.thingy();
 }
 
 //Used for first person mode
@@ -250,4 +268,91 @@ function serverToClientObject( %serverObject )
       return 0;
                 
    return ServerConnection.resolveGhostID( %ghostId );   
+}
+
+function ThirdPersonPlayerObject::writePacket(%this)
+{
+   //$hi += 1;
+   //%this.write($hi);
+   
+   %this.counter += 1;
+   if (%this.writeFlag(%this.counter > 20 && %this.pending))
+   {
+      %this.counter = 25;
+      %this.pending = false;
+      %this.writeString("thing");
+   }
+   //if(%this.counter > 22)
+   //   warn("something should be here by now");
+      
+   return false;
+}
+
+function ThirdPersonPlayerObject::readPacket(%this)
+{
+   //%this.methodTest();
+   //%hi = %this.read();
+   //echo(%hi);
+   
+   if(%this.readFlag())
+   {
+      warn("did the thing");
+      %this.namespaced = %this.readString();
+      %this.addNameSpace(%this.namespaced);
+   }
+}
+
+$pos= "0 0 0";
+$rot= "0 0 0";
+   
+function ThirdPersonPlayerObject::updateMove(%this, %move, %acc, %conNorm, %falling, %watercov, %mvel)
+{
+   //%moveVector = %this.getMoveVector();
+   
+   %moveVec = VectorRot(%move, -%this.getRotation().z);
+
+   %this.phys.setAccel(VectorAdd(%acc, VectorScale(%moveVec, 1)));
+   
+   //$diff = VectorSub($pos, %this.getPosition());
+   //$diffr = VectorSub($rot, %this.getRotation());
+   //if (VectorLen($diff) > 0 || VectorLen($diffr) > 0)
+   //{
+   //   echo("Position change :" SPC $diff);
+   //   echo("Rotation change :" SPC $diffr);
+   //}
+
+   //echo($diffr);
+   //$pos = %this.getPosition();
+   //$rot = %this.getRotation();
+}
+
+function thing::thingy()
+{
+   // echo(1);
+}
+
+function randomclass::Update()
+{
+   %this.owner.getComponent( PlayerControllerComponent ).velocity = getRandom() SPC getRandom() SPC "0";
+}
+
+function physicstime(%num)
+{
+   while(%num > 0)
+   {
+      new physicsplayerexample(){position = "6 -24 50" ; movevector = 1*(getrandom()-0.5) SPC 1*(getrandom()-0.5) SPC 50*getRandom();};
+      //%obj = SGOManager.spawn("ThirdPersonPlayerObject", false);
+      //%obj.position = "6 -24 5";
+      
+      //%comp = new Component()
+      //{
+      //   networked="false"; while(%i < 100){%thing = new physicsexample(){}; %thing.setPosition(getRandom() * 5 SPC getRandom() * 20 SPC getRandom() * 6 + 100); %thing.setVelocity( getRandom() SPC getRandom() SPC getRandom() * 100); %i++;}
+      //   enabled="true";
+      //   class="randomclass";
+      //};
+      //%this.addcomponent(%comp);
+      //%this.addNameSpace("aiPerson");
+      %num--;
+   }//15.1973 -35.9251 -63.9015
+   //%this.addNameSpace(%this.namespaced);
 }
